f6edcbd0fc2873eb0303c9c6a92e3b7956b1f405
MatsunoTaiga <tpinchan@gmail.com>
2020-10-13 20:54:36 +0900

    全てでやった

---
+++ FriedChicken/Assets/Script/TitleButtonSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TitleButtonSelect : MonoBehaviour
+{
+	[SerializeField] GameObject firstSelect;
+	[SerializeField] AudioClip cursorMove;
+	AudioSource source;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		source = GetComponent<AudioSource>();
+		EventSystem.current.SetSelectedGameObject(firstSelect);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			source.PlayOneShot(cursorMove);
+		}
+	}
+}
